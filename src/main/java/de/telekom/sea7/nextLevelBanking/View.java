0faa7a6a3e7a0b8f5947fa82a6bf6b2f581dfc2b
Oliver Reiger <oliver.reiger@mac.com>
2022-01-19 16:42:00 +0100

    Kleines Update. Erweiterung des Menüs der Banking Übung um einen Testdaten Generator!

--- src/main/java/de/telekom/sea7/nextLevelBanking/View.java
+++ src/main/java/de/telekom/sea7/nextLevelBanking/View.java
@@ -9,117 +9,103 @@ public class View {
 	// - Ausgabe Methoden
 	// - Methode zur Erzeugung einer neuen Transaktion
 	// - ArrayList zur Speicherung mehrerer Single Transaktionen
-	
-	
 
-	ArrayList <Object> banking = new ArrayList <>(); 
-	
+	ArrayList<Object> banking = new ArrayList<>();
+
 	private VorlageUeberweisung vorlageUeberweisung;
-		
-	
+
 	// Menü
 	public void menu() {
 		String eingabe = "";
 		Scanner scanner = new Scanner(System.in);
-		
-		while(!eingabe.equals("exit")) {
+
+		while (!eingabe.equals("exit")) {
 			System.out.println("");
-			System.out.println("Bitte gib eine Aktion ein (z.B. show, showAll, exit, input)");
+			System.out.println("Bitte gib eine Aktion ein (z.B. show, showAll, exit, input, testDataGen)");
 			eingabe = scanner.nextLine();
-			
+
 			switch (eingabe) {
-			case "show":	
+			case "show":
 				System.out.println("Du hast dich für die Anzeige einer Überweisung entschieden. Hier bitte: \n ");
 				ausgeben();
-					break;
+				break;
 			case "input":
 				eingeben();
 				break;
 			case "showAll":
 				allesAusgeben();
 				break;
-			case "exit": 
+			case "testDataGen":
+				testDatenHinzufuegen();
+				break;	
+			case "exit":
 				System.out.println("Schön, dass du da warst. Tschüss \n ");
 			}
-			
+			// scanner.close();
 		}
-		
-		
-		
-		
-		
+
 	}
-	
-	
-	
+
 	// Methoden die aus dem Menü aufgerufen werden können
-	
+
 	public void ausgeben() {
-		System.out.println(
-				"Empfänger: " +
-				vorlageUeberweisung.getEmpfaenger() +
-				"\nIBAN Empfänger: " +
-				vorlageUeberweisung.getEmpfaengerIban() +
-				"\nBIC Empfänger: " +
-				vorlageUeberweisung.getEmpfaengerBic() +
-				"\nBetrag: " +
-				vorlageUeberweisung.getBetrag() + " " +
-				vorlageUeberweisung.getWaehrung() +
-				"\nVerwendungszweck: " +
-				vorlageUeberweisung.getVerwendungszweck());
+		System.out.println("Empfänger: " + vorlageUeberweisung.getEmpfaenger() + "\nIBAN Empfänger: "
+				+ vorlageUeberweisung.getEmpfaengerIban() + "\nBIC Empfänger: " + vorlageUeberweisung.getEmpfaengerBic()
+				+ "\nBetrag: " + vorlageUeberweisung.getBetrag() + " " + vorlageUeberweisung.getWaehrung()
+				+ "\nVerwendungszweck: " + vorlageUeberweisung.getVerwendungszweck());
 	}
 
 	public void eingeben() {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("Bitte gib einen Empfänger an:");
 		String eingabeEmpfaenger = scanner.nextLine();
-		
+
 		System.out.println("Bitte gib eine IBAN an:");
 		String eingabeIban = scanner.nextLine();
-		
+
 		System.out.println("Bitte gib eine BIC an:");
 		String eingabeBic = scanner.nextLine();
-		
+
 		System.out.println("Bitte gib einen Betrag an:");
 		String eingabeBetrag1 = scanner.nextLine();
 		double eingabeBetrag = Double.parseDouble(eingabeBetrag1);
-		
+
 		System.out.println("Bitte gib die Währung an:");
 		String eingabeWaehrung = scanner.nextLine();
-		
+
 		System.out.println("Bitte gib den Verwendungszweck an:");
 		String eingabeZweck = scanner.nextLine();
-		
-				
-		VorlageUeberweisung sTm = new VorlageUeberweisung(eingabeEmpfaenger, eingabeIban, eingabeBic, eingabeBetrag, eingabeWaehrung, eingabeZweck);
+
+		VorlageUeberweisung sTm = new VorlageUeberweisung(eingabeEmpfaenger, eingabeIban, eingabeBic, eingabeBetrag,
+				eingabeWaehrung, eingabeZweck);
 		banking.add(sTm); // Hinzufügen der neu erzeugten Transaktion in die ArrayList
-		
+
 		System.out.println("");
 		System.out.println("Zur Kontrolle, hier deine eigegebenen Daten:");
-		
-		System.out.println(
-				"Empfänger: " +
-				sTm.getEmpfaenger() +
-				"\nIBAN Empfänger: " +
-				sTm.getEmpfaengerIban() +
-				"\nBIC Empfänger: " +
-				sTm.getEmpfaengerBic() +
-				"\nBetrag: " +
-				sTm.getBetrag() + " " +
-				sTm.getWaehrung() +
-				"\nVerwendungszweck: " +
-				sTm.getVerwendungszweck());
+
+		System.out.println("Empfänger: " + sTm.getEmpfaenger() + "\nIBAN Empfänger: " + sTm.getEmpfaengerIban()
+				+ "\nBIC Empfänger: " + sTm.getEmpfaengerBic() + "\nBetrag: " + sTm.getBetrag() + " "
+				+ sTm.getWaehrung() + "\nVerwendungszweck: " + sTm.getVerwendungszweck());
+		// scanner.close();
 	}
-	
-	// Ausgabe aller vom User erstellten Transaktionen die sich in der ArrayList befinden
+
+	// Ausgabe aller vom User erstellten Transaktionen die sich in der ArrayList
+	// befinden
 	public void allesAusgeben() {
-		for(Object ausgabe : banking) {
+		for (Object ausgabe : banking) {
 			System.out.println(ausgabe);
 		}
 	}
-	
 
+	public void testDatenHinzufuegen() {
+		VorlageUeberweisung testdaten1 = new VorlageUeberweisung("Empfaenger_1", "DE12TESTIBAN111", "BIC111", 10.5, "€", "Verwendung_1");
+		banking.add(testdaten1);
+		VorlageUeberweisung testdaten2 = new VorlageUeberweisung("Empfaenger_2", "DE12TESTIBAN222", "BIC222", 11.5, "€", "Verwendung_2");
+		banking.add(testdaten2);
+		VorlageUeberweisung testdaten3 = new VorlageUeberweisung("Empfaenger_3", "DE12TESTIBAN333", "BIC333", 12.5, "€", "Verwendung_3");
+		banking.add(testdaten3);
+	}
 	
 	public VorlageUeberweisung getsT() {
 		return vorlageUeberweisung;
@@ -128,8 +114,5 @@ public class View {
 	public void setsT(VorlageUeberweisung vorlageUeberweisung) {
 		this.vorlageUeberweisung = vorlageUeberweisung;
 	}
-	
-
 
 }
-
--- src/main/java/de/telekom/sea7/nextLevelBanking/VorlageUeberweisung.java
+++ src/main/java/de/telekom/sea7/nextLevelBanking/VorlageUeberweisung.java
@@ -6,18 +6,18 @@ public class VorlageUeberweisung {
 	// - Get-Methoden zum Auslesen einzlener Parameter
 	// - Set-Methoden zum Setzen einzelner Parameter
 	// - Constructor zum Erzeugen eines Objekts mit den einzelnen Parametern
-	
-	
+
 	// Properties
-	private String 	empfaenger;
-	private String 	empfaengerIban;
-	private String 	empfaengerBic;
-	private double 	betrag;
-	private String	waehrung;
-	private String	verwendungszweck;
-	
+	private String empfaenger;
+	private String empfaengerIban;
+	private String empfaengerBic;
+	private double betrag;
+	private String waehrung;
+	private String verwendungszweck;
+
 	// Eigener Constructor
-	public VorlageUeberweisung(String empfaenger, String empfaengerIban, String empfaengerBic, double betrag, String waehrung, String verwendungszweck) {
+	public VorlageUeberweisung(String empfaenger, String empfaengerIban, String empfaengerBic, double betrag,
+			String waehrung, String verwendungszweck) {
 		this.empfaenger = empfaenger;
 		this.empfaengerIban = empfaengerIban;
 		this.empfaengerBic = empfaengerBic;
@@ -25,54 +25,61 @@ public class VorlageUeberweisung {
 		this.waehrung = waehrung;
 		this.verwendungszweck = verwendungszweck;
 	}
-	
+
 	// Aufgehübschte toString Methode für bessere hübschere Ausgaben
 	public String toString() {
-		return "Empfänger: " + this.empfaenger +
-				" Empfänger IBAN: " + this.empfaengerIban + 
-				" Empfänger BIC: " + this.empfaengerBic + 
-				" Betrag: " + this.betrag + " " + this.waehrung +
-				" Verwendungszweck: " + this.verwendungszweck;
+		return "Empfänger: " + this.empfaenger + " | Empfänger IBAN: " + this.empfaengerIban + " | Empfänger BIC: "
+				+ this.empfaengerBic + " | Betrag: " + this.betrag + " " + this.waehrung + " | Verwendungszweck: "
+				+ this.verwendungszweck;
 	}
-	
+
 	// Get-/Set-Methoden
 	public String getEmpfaenger() {
 		return empfaenger;
 	}
+
 	public void setEmpfaenger(String empfaenger) {
 		this.empfaenger = empfaenger;
 	}
+
 	public String getEmpfaengerIban() {
 		return empfaengerIban;
 	}
+
 	public void setEmpfaengerIban(String empfaengerIban) {
 		this.empfaengerIban = empfaengerIban;
 	}
+
 	public String getEmpfaengerBic() {
 		return empfaengerBic;
 	}
+
 	public void setEmpfaengerBic(String empfaengerBic) {
 		this.empfaengerBic = empfaengerBic;
 	}
+
 	public double getBetrag() {
 		return betrag;
 	}
+
 	public void setBetrag(double betrag) {
 		this.betrag = betrag;
 	}
+
 	public String getWaehrung() {
 		return waehrung;
 	}
+
 	public void setWaehrung(String waehrung) {
 		this.waehrung = waehrung;
 	}
+
 	public String getVerwendungszweck() {
 		return verwendungszweck;
 	}
+
 	public void setVerwendungszweck(String verwendungszweck) {
 		this.verwendungszweck = verwendungszweck;
 	}
-	
-	
 
 }
